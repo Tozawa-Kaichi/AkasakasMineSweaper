8cdaa0faa101a88ef63ed64553c0dc22227abd37
戸澤海智 <st37071556@icloud.com>
2022-05-09 23:24:18 +0900

    sell enum 3

--- Assets/Script/CellCon.cs
+++ Assets/Script/CellCon.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CellCon : MonoBehaviour
 {
     [SerializeField]int _cellZize = 0;
-    enum CellType
+    enum CellType {Empty,Mine,Count }
     // Start is called before the first frame update
     void Start()
     {
