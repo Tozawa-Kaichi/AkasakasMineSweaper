8c8feae1e9b6987ee98bd97205436c7dedff3101
戸澤海智 <st37071556@icloud.com>
2022-05-09 23:22:25 +0900

    cellcon

--- Assets/Script/CellCon.cs
+++ Assets/Script/CellCon.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CellCon : MonoBehaviour
 {
     [SerializeField]int _cellZize = 0;
+    enum CellType
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +20,11 @@ public class CellCon : MonoBehaviour
 
     void CellCheck()//セルが地雷かどうか調べる
     {
+        //安全なセルの場合
 
+        //地雷セルの場合
+
+        //地雷の隣の場合
     }
     void MineCount()//周囲の地雷数を表示
     {
