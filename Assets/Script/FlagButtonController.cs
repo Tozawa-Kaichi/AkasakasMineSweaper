e82a5121e8c0988008a15d6149a3a779d58661cc
戸澤海智 <st37071556@icloud.com>
2022-05-11 13:31:06 +0900

    flagbutton

--- Assets/Script/FlagButtonController.cs
+++ Assets/Script/FlagButtonController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;//これがないとIPointerClickHandlerは動かないからusingに追加しよう
 //自分で右クリックに反応するボタンを作成する
-public class FlagButtonController : MonoBehaviour, IPointerClickHandler  //これをモノビヘィビァーの後ろに追加する
+public class FlagButtonController : MonoBehaviour, IPointerClickHandler  //これをモノビヘィビァーの後ろに追加する 
 {
     public void OnPointerClick(PointerEventData eventData)
     {
