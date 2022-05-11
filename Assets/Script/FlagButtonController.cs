d19039a448827c45df938cbdd9980bbdc2272bd7
戸澤海智 <st37071556@icloud.com>
2022-05-11 15:10:59 +0900

    右、左、ホイールクリックの処理を分けて独自に設定できる術を学んだ これは自作ゲームにりようでっきそうだ

--- Assets/Script/FlagButtonController.cs
+++ Assets/Script/FlagButtonController.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;//これがないとIPointerClickHandlerは動かないからusingに追加しよう
 //自分で右クリックに反応するボタンを作成する
-public class FlagButtonController : MonoBehaviour, IPointerClickHandler//IPointerClickHandlerを継承する
+public class FlagButtonController : MonoBehaviour, IPointerClickHandler//IPointerClickHandler（インターフェース）を継承する
 {
-    public void OnPointerClick(PointerEventData eventData)
+    public void OnPointerClick(PointerEventData eventData)//IPointerClickHandler（インターフェース）
     {
-        throw new System.NotImplementedException();
+        switch (eventData.button)
+        {
+            case PointerEventData.InputButton.Left:
+                //左クリックの処理
+                break;
+            case PointerEventData.InputButton.Right:
+                //右クリックの処理
+                break;
+            case PointerEventData.InputButton.Middle:
+                //（ホイールクリックの処理、は）無いです
+                break;
+        }
     }
 
     // Start is called before the first frame update
